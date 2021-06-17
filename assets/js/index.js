2e5efd5813e34bb211c3527b7b469bd63ef3586e
Anurag Singh <anuragsinghbam@gmail.com>
2021-06-18 01:13:45 +0530

    add user page

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,10 @@ const searchForm = document.querySelector('.search-form')
 const expenseForm = document.querySelector('.expense-form')
 const addButton = document.querySelector('.add-expense')
 const overlay = document.querySelector('.overlay')
+const userProfile = document.querySelector('.user-profile')
+const userIcon = document.querySelector('.user')
+const backButton = document.querySelector('.back-button')
+const homepage = document.querySelector('.homepage')
 
 searchInput.addEventListener('focus', (e) => {
   searchForm.classList.add('focused')
@@ -18,3 +22,16 @@ overlay.addEventListener('click', (e) => {
   overlay.classList.remove('active')
 })
 
+
+userIcon.addEventListener('click', (e) => {
+  userProfile.classList.add('active')
+  setTimeout(() => {
+    homepage.classList.add('hidden')
+  }, 200)
+})
+
+
+backButton.addEventListener('click', (e) => {
+  homepage.classList.remove('hidden')
+  userProfile.classList.remove('active')
+})
--- index.js
+++ index.js
@@ -74,6 +74,7 @@ app.use((req, res, next) => {
   next()
 })
 
+
 app.get('/', async (req, res) => {
   if(req.user) {
     return res.redirect(`/${req.user.username}`)
